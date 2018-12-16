0b96d4ba172d4e1edd3f1b2d808ee1f647418d8e
Sarah Harrington <sarah.soberg@gmail.com>
2018-12-15 22:35:02 -0600

    pieces displaying based on gamestate from server

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -3,31 +3,51 @@ console.log('javascript loaded');
 const socket = io();
 //Board Things
 
-let allSpaces = document.querySelectorAll('.playable');
-
-console.log('allthespaces', allSpaces);
-
+let boardSpaces = document.querySelectorAll('.playable');
+// let startGame = document.querySelector('#start-game').addEventListener('startGame', startGame);
 
 socket.on('aNewClientConnection', (id) => {
   console.log(id);
-  
+
 })
 
 socket.on('gameStarted', (game) => {
-  console.log(game);
+  console.log('two players have joined');
   displayGame(game);
 })
 
 function displayGame(game) {
-  for (let i = 0; i < game.state; i++) {
-    if (game.state[i].player = 1) {
-      
+  console.log('in the game load', game.state)
+  for (let i = 0; i < game.state.length; i++) {
+    // console.log(game.state[i])
+    console.log('in the loop!')
+    if (game.state[i].player === 1) {
+      let playerOnePiece = document.createElement('div');
+      playerOnePiece.classList.add('game-piece');
+      playerOnePiece.classList.add('player-one-piece');
+      playerOnePiece.setAttribute('id', 'p1');
+      boardSpaces[i].appendChild(playerOnePiece);
+    }
+    if (game.state[i].player === 2) {
+      let playerOnePiece = document.createElement('div');
+      playerOnePiece.classList.add('game-piece');
+      playerOnePiece.classList.add('player-two-piece');
+      playerOnePiece.setAttribute('id', 'p2');
+      boardSpaces[i].appendChild(playerOnePiece);
     }
   }
 }
 
+// function startGame() {
+
+// }
 
 
+// let playerOnePiece = document.createElement('div');
+// playerOnePiece.classList.add('game-piece');
+// playerOnePiece.classList.add('player-one-piece');
+// playerOnePiece.setAttribute('id', 'p1');
+// boardSpaces[i].appendChild(playerOnePiece);
 
 /***** KEEPS CONNECTION ALIVE FOR BROWSERS THAT TIME OUT Tabs*****/
 //TODO: If I use a cookie, could I track the user somehow?
